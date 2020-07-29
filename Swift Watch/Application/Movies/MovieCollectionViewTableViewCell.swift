3b0947b32231839394d74157e9f7804617f75d3d
Victor Ragojos <vhsvragojos@gmail.com>
2020-07-29 11:24:19 -0700

    ADD: CollectionView && SectionHeader

---
+++ Swift Watch/Application/Movies/MovieCollectionViewTableViewCell.swift
@@ -0,0 +1,97 @@
+//
+//  MovieCollectionViewTableViewCell.swift
+//  Swift Watch
+//
+//  Created by Victor Ragojos on 7/27/20.
+//  Copyright © 2020 Victor Ragojos. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewTableViewCell: UITableViewCell {
+    var data: MovieSection?
+    
+    lazy var collectionView: UICollectionView = {
+        // Setup Layout
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 15
+        layout.scrollDirection = .horizontal
+        
+        // Setup CollectionView
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.clipsToBounds = true
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        collectionView.register(MovieCell.self, forCellWithReuseIdentifier: MovieCell.reuseIdentifier)
+        return collectionView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = .clear
+        addSubview(collectionView)
+        
+        collectionView.topAnchor.constraint(equalTo: topAnchor , constant: 10).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true
+        collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 0.5).isActive = true
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func configure(_ movieSection: MovieSection) {
+        data = movieSection
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MovieCollectionViewTableViewCell: UICollectionViewDelegate {
+}
+
+extension MovieCollectionViewTableViewCell: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let cell = collectionView.cellForItem(at: indexPath) as? MovieCell else { return }
+        cell.pulse()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if let count = data?.results?.count {
+            return count
+        }
+        
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieCell.reuseIdentifier, for: indexPath) as! MovieCell
+        
+        if let movie = data?.results?[indexPath.row] {
+            cell.configure(with: movie)
+        }
+        return cell
+    }
+}
+
+extension MovieCollectionViewTableViewCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 100, height: collectionView.frame.width / 2)
+    }
+}
---
+++ Swift Watch/Application/Movies/MovieSectionHeader.swift
@@ -0,0 +1,47 @@
+//
+//  MovieHeaderCollectionReusableView.swift
+//  Swift Watch
+//
+//  Created by Victor Ragojos on 7/26/20.
+//  Copyright © 2020 Victor Ragojos. All rights reserved.
+//
+
+import UIKit
+
+class MovieSectionHeader: UIView {
+    lazy var title: UILabel = {
+        let title = UILabel()
+        title.textColor = UIColor(named: "primaryTextColor")
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.font = UIFontMetrics.default.scaledFont(for: UIFont.systemFont(ofSize: 22, weight: .black))
+        
+        return title
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+    
+    func configure(with text: String) {
+        title.text = text.uppercased()
+    }
+    
+    private func setupUI() {
+        backgroundColor = .clear
+        
+        addSubview(title)
+        
+        NSLayoutConstraint.activate([
+            heightAnchor.constraint(equalToConstant: 65),
+            title.heightAnchor.constraint(equalToConstant: 30),
+            title.bottomAnchor.constraint(equalTo: bottomAnchor),
+            title.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
