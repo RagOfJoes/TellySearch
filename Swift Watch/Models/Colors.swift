86caf0f30aaca35a306f39491fd330675ce17850
Victor Ragojos <vhsvragojos@gmail.com>
2020-08-11 14:03:32 -0700

    Revert "Revert "ADD: UIImageColors library""
    This reverts commit 78ac01eebd6cacdcf0473d66859d9828200d91bb.

---
+++ Swift Watch/Models/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  Swift Watch
+//
+//  Created by Victor Ragojos on 8/5/20.
+//  Copyright © 2020 Victor Ragojos. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+struct Colors: Codable {
+    let background: CGColor
+    let primary: CGColor
+    let secondary: CGColor
+    let detail: CGColor
+}
