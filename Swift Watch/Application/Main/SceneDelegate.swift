b9e821614997a577be784900e9e5289b7c3c3837
Victor Ragojos <vhsvragojos@gmail.com>
2020-08-11 13:48:20 -0700

    ADD: default Skeleton appearance

--- Swift Watch/Application/Main/SceneDelegate.swift
+++ Swift Watch/Application/Main/SceneDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SkeletonView
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
@@ -16,7 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
+        SkeletonAppearance.default.gradient = SkeletonGradient(baseColor: .systemGray4, secondaryColor: .systemGray2)
+
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         
