b27d6444d600a62a055405541ec51d9fbf035b69
Victor Ragojos <vhsvragojos@gmail.com>
2020-09-21 23:57:26 -0700

    ADD: Reusable CollectionViews

---
+++ Swift Watch/Common/Views/CVTCell.swift
@@ -0,0 +1,129 @@
+//
+//  CVTCell.swift
+//  Swift Watch
+//
+//  Created by Victor Ragojos on 9/21/20.
+//  Copyright © 2020 Victor Ragojos. All rights reserved.
+//
+
+import UIKit
+import SkeletonView
+
+enum CVTCellType {
+    case Regular
+    case Featured
+}
+
+class CVTCell: UITableViewCell {
+    static var reuseIdentifier: String {
+        get {
+            return String(describing: self)
+        }
+    }
+    
+    // MARK: - Internal Properties
+    private var type: CVTCellType!
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView.createHorizontalCollectionView(minimumLineSpacing: 20)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(RegularCell.self, forCellWithReuseIdentifier: RegularCell.reuseIdentifier)
+        collectionView.register(FeaturedCell.self, forCellWithReuseIdentifier: FeaturedCell.reuseIdentifier)
+        
+        return collectionView
+    }()
+    
+    // MARK: - Lifecycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: CVTCell.reuseIdentifier)
+        clipsToBounds = true
+        backgroundColor = .clear
+        
+        contentView.addSubview(self.collectionView)
+        
+        setupAnchors()
+        isSkeletonable = true
+        
+        self.collectionView.prepareSkeleton { [weak self] (done) in
+            self?.showAnimatedGradientSkeleton()
+        }
+    }
+    
+    public func configure(_ type: CVTCellType) {
+        self.type = type
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Helper Functions
+extension CVTCell {
+    private func setupAnchors() {
+        var collectionViewHeight: NSLayoutConstraint!
+        var collectionViewLeading: NSLayoutConstraint!
+        var collectionViewTrailing: NSLayoutConstraint!
+        
+        if #available(iOS 11, *) {
+            collectionViewLeading = collectionView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor)
+            collectionViewTrailing = collectionView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor)
+        } else {
+            collectionViewLeading = collectionView.leadingAnchor.constraint(equalTo: leadingAnchor)
+            collectionViewTrailing = collectionView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        }
+        
+        if self.type == .Featured {
+            collectionViewHeight = collectionView.heightAnchor.constraint(equalToConstant: K.Overview.featuredCellHeight)
+        } else {
+            collectionViewHeight = collectionView.heightAnchor.constraint(equalToConstant: K.Overview.regularHeight)
+        }
+        
+        let collectionViewConstraints: [NSLayoutConstraint] = [
+            collectionViewHeight,
+            collectionViewLeading,
+            collectionViewTrailing,
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+        ]
+        NSLayoutConstraint.activate(collectionViewConstraints)
+    }
+}
+
+extension CVTCell: SkeletonCollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionSkeletonView(_ skeletonView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionSkeletonView(_ skeletonView: UICollectionView, cellIdentifierForItemAt indexPath: IndexPath) -> ReusableCellIdentifier {
+        if self.type == .Featured {
+            return FeaturedCell.reuseIdentifier
+        } else {
+            return RegularCell.reuseIdentifier
+        }
+    }
+}
+
+// MARK: - UICollectionViewLayout
+extension CVTCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var width: CGFloat = K.Poster.width
+        
+        if type == .Featured {
+            width = K.Overview.featuredCellWidth
+        }
+        
+        return CGSize(width: width, height: collectionView.frame.height)
+    }
+}
---
+++ Swift Watch/Common/Views/GenericCollectionView.swift
@@ -0,0 +1,161 @@
+//
+//  GenericCollectionView.swift
+//  Swift Watch
+//
+//  Created by Victor Ragojos on 9/20/20.
+//  Copyright © 2020 Victor Ragojos. All rights reserved.
+//
+
+import UIKit
+import SkeletonView
+
+enum GenericCollectionViewType {
+    case Featured
+    
+    case Regular
+    case RegularHasSecondary
+}
+
+class GenericCollectionView: UIView {
+    // MARK: - Internal Properties
+    private let type: GenericCollectionViewType!
+    
+    private lazy var header: GenericCollectionViewHeader = {
+        let header = GenericCollectionViewHeader()
+        header.translatesAutoresizingMaskIntoConstraints = false
+        header.isSkeletonable = true
+        return header
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView.createHorizontalCollectionView(minimumLineSpacing: 10)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Only Register the Cell's that are required
+        if self.type == .Featured {
+            collectionView.register(FeaturedCell.self, forCellWithReuseIdentifier: FeaturedCell.reuseIdentifier)
+        } else {
+            collectionView.register(RegularCell.self, forCellWithReuseIdentifier: RegularCell.reuseIdentifier)            
+        }
+        
+        return collectionView
+    }()
+    
+    // MARK: - Lifecycle
+    init(_ type: GenericCollectionViewType) {
+        self.type = type
+        super.init(frame: .zero)
+        clipsToBounds = true
+        backgroundColor = .clear
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(self.header)
+        addSubview(self.collectionView)
+        
+        setupAnchors()
+        isSkeletonable = true
+        
+        self.collectionView.prepareSkeleton { [weak self] (done) in
+            self?.showAnimatedGradientSkeleton()
+        }
+    }
+    
+    public func setupHeader(title: String, color: UIColor? = UIColor(named: "primaryTextColor")) {
+        self.header.configure(title, color: color)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Constraints
+extension GenericCollectionView {
+    private func setupAnchors() {
+        var heightConstraint: NSLayoutConstraint!
+        var collectionViewHeight: NSLayoutConstraint!
+        
+        let regularHeightConstraint = heightAnchor.constraint(equalToConstant: K.Overview.regularHeight + 35)
+        let regularHeightSecondaryConstraint = heightAnchor.constraint(equalToConstant: K.Overview.regularHeightWithSecondary + 35)
+        let featuredHeightConstraint = heightAnchor.constraint(equalToConstant: K.Overview.featuredCellHeight - 100)
+        
+        switch self.type {
+        case .Featured:
+            heightConstraint = featuredHeightConstraint
+            collectionViewHeight = collectionView.heightAnchor.constraint(equalToConstant: featuredHeightConstraint.constant)
+            break
+        case .RegularHasSecondary:
+            heightConstraint = regularHeightSecondaryConstraint
+            collectionViewHeight = collectionView.heightAnchor.constraint(equalToConstant: regularHeightSecondaryConstraint.constant)
+            break
+        default:
+            heightConstraint = regularHeightConstraint
+            collectionViewHeight = collectionView.heightAnchor.constraint(equalToConstant: regularHeightConstraint.constant)
+        }
+        
+        NSLayoutConstraint.activate([
+            heightConstraint,
+            
+            header.topAnchor.constraint(equalTo: topAnchor),
+            header.heightAnchor.constraint(equalToConstant: 30),
+            header.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            header.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+        ])
+        
+        var collectionViewLeading: NSLayoutConstraint!
+        var collectionViewTrailing: NSLayoutConstraint!
+        
+        if #available(iOS 11, *) {
+            collectionViewLeading = collectionView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor)
+            collectionViewTrailing = collectionView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor)
+        } else {
+            collectionViewLeading = collectionView.leadingAnchor.constraint(equalTo: leadingAnchor)
+            collectionViewTrailing = collectionView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        }
+        
+        let collectionViewConstraints: [NSLayoutConstraint] = [
+            collectionViewHeight,
+            collectionViewLeading,
+            collectionViewTrailing,
+            collectionView.topAnchor.constraint(equalTo: header.bottomAnchor, constant: 2)
+        ]
+        NSLayoutConstraint.activate(collectionViewConstraints)
+    }
+}
+
+extension GenericCollectionView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var width: CGFloat = K.Poster.width
+        
+        if type == .Featured {
+            width = K.Overview.featuredCellWidth
+        }
+        
+        return CGSize(width: width, height: collectionView.frame.height)
+    }
+}
+
+// MARK: - SkeletonCollectionViewDelegate
+extension GenericCollectionView: SkeletonCollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionSkeletonView(_ skeletonView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionSkeletonView(_ skeletonView: UICollectionView, cellIdentifierForItemAt indexPath: IndexPath) -> ReusableCellIdentifier {
+        if self.type == .Featured {
+            return FeaturedCell.reuseIdentifier
+        } else {
+            return RegularCell.reuseIdentifier
+        }
+    }
+}
